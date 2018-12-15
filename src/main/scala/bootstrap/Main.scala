3f02b970d03d00444a78724e3d6567af76e05210
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-12-15 12:23:08 +0900

    bug fix

--- src/main/scala/bootstrap/Main.scala
+++ src/main/scala/bootstrap/Main.scala
@@ -4,7 +4,9 @@ import play.api.libs.json._
 import scalaj.http._
 
 object Main {
-  
+
+  implicit val requestReads = Json.reads[Request]
+
   def main(args: Array[String]): Unit = {
     val handler  = System.getenv("_HANDLER")
     val taskRoot = System.getenv("LAMBDA_TASK_ROOT")
@@ -20,22 +22,29 @@ object Main {
         Http(s"http://$runtime/2018-06-01/runtime/init/error").postData(message).asString
     }
 
+    val HttpResponse(jsResult, _, headers) =
+      Http(s"http://$runtime/2018-06-01/runtime/invocation/next").execute(x => Json.parse(x).validate[Request])
+
+    val requestId = headers.get("lambda-runtime-aws-request-id")
+    val traceId = headers.get("lambda-runtime-trace-id")
+
+    println(s"body: $jsResult")
+    println(s"headers: $headers")
+
     while (true) {
-      val HttpResponse(jsResult, _, headers) =
-        Http(s"http://$runtime/2018-06-01/runtime/invocation/next").execute(x => Json.parse(x).validate[Request])
-
-      val requestId = headers.get("lambda-runtime-aws-request-id")
-      val traceId = headers.get("lambda-runtime-trace-id")
-
-      println(s"body: $jsResult")
-      println(s"headers: $headers")
-
       try {
-        val name = jsResult.get.name
-        val result = Json.obj("message" -> s"Hello, $name!").toString
+        val result = Json.obj(
+          "headers" -> ("content-type" -> "text"),
+          "isBase64Encoded" -> false,
+          "statusCode" -> 200,
+          "body" -> s"Hello, ${jsResult.get.name}!"
+        ).toString
+        println(s"result: $result")
         Http(s"http://$runtime/2018-06-01/runtime/invocation/$requestId/response").postData(result).asString
       } catch {
         case e: Exception =>
+          println(e.getClass.getName)
+          println(e.getMessage)
           val message = Json.obj("errorMessage" -> e.getMessage, "errorType" -> e.getClass.getName).toString
           Http(s"http://$runtime/2018-06-01/runtime/invocation/$requestId/error").postData(message).asString
       }
